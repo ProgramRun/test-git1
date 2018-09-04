968e8c89f7e3e886e553ad61da5197fd2721f229
zad <zhaoandong@yeah.net>
2018-09-04 22:11:48 +0800

    add a project to github

---
+++ Test-RMI/src/test/java/com/zad/RMI/RMITest.java
@@ -0,0 +1,24 @@
+package com.zad.RMI;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 描述:
+ * zzz
+ *
+ * @author zad
+ * @create 2018-09-04 21:58
+ */
+@Slf4j
+public class RMITest {
+    @Test
+    public void t1() {
+        System.out.println("hahah");
+    }
+
+    @Test
+    public void t2() {
+        log.info("haha");
+    }
+}
