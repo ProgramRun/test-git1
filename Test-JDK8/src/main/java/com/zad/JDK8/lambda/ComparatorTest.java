4db9c8a281b5d97ff62bb75ef1bf26d74077a5cb
zad <zhaoandong@yeah.net>
2018-09-09 22:51:21 +0800

    lambda

---
+++ Test-JDK8/src/main/java/com/zad/JDK8/lambda/ComparatorTest.java
@@ -0,0 +1,16 @@
+package com.zad.JDK8.lambda;
+
+import java.util.Arrays;
+
+/**
+ * 描述:
+ * ComparatorTest
+ *
+ * @author zad
+ * @create 2018-09-09 22:46
+ */
+public class ComparatorTest {
+    public static void main(String[] args) {
+
+    }
+}
