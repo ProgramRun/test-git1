28ec2e1748909eeb987381333f50c2e25e35848e
赵安东 <zad@ipms.cn>
2019-10-22 15:27:40 +0800

    zad 添加工具类CollectionUtil重构

--- Test-JDK8/src/main/java/com/zad/jdk8/util/CollectionUtil.java
+++ Test-JDK8/src/main/java/com/zad/jdk8/util/CollectionUtil.java
@@ -1,14 +1,13 @@
 package com.zad.jdk8.util;
 
-import com.google.common.base.Function;
-import com.google.common.base.Strings;
+
 import com.google.common.collect.Lists;
 
 import java.util.*;
 
 /**
- * @author Binbin Wang
- * @date 2018/1/29
+ * @author zad
+ * @date 20191022
  */
 public final class CollectionUtil {
 
@@ -30,180 +29,142 @@ public final class CollectionUtil {
         return !isEmpty(map);
     }
 
-    public static List<String> removeRepeatElements(List<String> list) {
-        Set<String> set = new HashSet<>(list);
-        return new ArrayList<>(set);
-    }
-
-    public static List<String> removeBlankElements(List<String> list) {
-        List<String> result = Lists.newArrayList();
-        for (String s : list) {
-            if (!Strings.isNullOrEmpty(s)) {
-                result.add(s);
+    public static <T> Collection<T> removeRepeatElements(Collection<T> input) {
+        if (isEmpty(input)) {
+            return input;
+        }
+        Set<T> res = new HashSet<>(input.size());
+        for (T entity : input) {
+            if (entity != null) {
+                res.add(entity);
             }
         }
-        return result;
+        return  res;
+    }
+
+    public static <T> Collection<T> removeNullElements(Collection<T> input) {
+        return filter(input, new Predict<T>() {
+            @Override
+            public boolean test(T t) {
+                return t == null;
+            }
+        });
     }
 
 
-    public static <F, T> List<T> convertList(List<F> req, Function<F, T> function) {
-        if (isEmpty(req)) {
-            return Collections.emptyList();
+    public static Collection<String> removeBlankString(Collection<String> input) {
+        return filter(input, new Predict<String>() {
+            @Override
+            public boolean test(String s) {
+                return StringUtil.isBlank(s);
+            }
+        });
+    }
+
+
+    public static <T> Collection<T> filter(Collection<T> input, Predict<T> predict) {
+        if (isEmpty(input)) {
+            return input;
         }
-        List<T> res = new ArrayList<>(req.size());
-        for (F entity : req) {
+
+        Iterator<T> it = input.iterator();
+
+        while (it.hasNext()) {
+            T temp = it.next();
+            if (predict.test(temp)) {
+                it.remove();
+            }
+        }
+        return input;
+    }
+
+
+    public static <K, V> Map<K, V> filterKey(Map<K, V> input, Predict<K> keyPredict) {
+        if (isEmpty(input)) {
+            return input;
+        }
+
+        Iterator<K> it = input.keySet().iterator();
+        while (it.hasNext()) {
+            K tempK = it.next();
+            if (keyPredict.test(tempK)) {
+                it.remove();
+            }
+        }
+        return input;
+    }
+
+    public static <K, V> Map<K, V> filterValue(Map<K, V> input, Predict<V> valuePredict) {
+        if (isEmpty(input)) {
+            return input;
+        }
+        Iterator<Map.Entry<K, V>> it = input.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<K, V> entry = it.next();
+            if (valuePredict.test(entry.getValue())) {
+                it.remove();
+            }
+        }
+        return input;
+    }
+
+
+    public static <T, U> Collection<U> convert(Collection<T> input, Function<T, U> function) {
+        if (isEmpty(input)) {
+            return Collections.EMPTY_LIST;
+        }
+
+        List<U> res = new ArrayList<>(input.size());
+        for (T entity : input) {
+            if (entity == null) {
+                continue;
+            }
             res.add(function.apply(entity));
         }
         return res;
     }
 
 
-    public static <F, T> Map<T, F> convert2NormalMap(List<F> list, Function<F, T> function) {
-        if (isEmpty(list)) {
+    public static <K, V, T> Map<K, V> convert2Map(Collection<T> input, Function<T, K> keyFunction,
+                                                  Function<T, V> valueFunction) {
+        if (isEmpty(input)) {
             return Collections.EMPTY_MAP;
         }
 
-        Map map = new HashMap<>(list.size());
-        for (F en : list) {
-            T t = function.apply(en);
-            map.put(t, en);
+        Map<K, V> res = new HashMap<>(input.size());
+        for (T entity : input) {
+            if (entity == null) {
+                continue;
+            }
+            K k = keyFunction.apply(entity);
+            V v = valueFunction.apply(entity);
+            res.put(k, v);
         }
-        return map;
+        return res;
     }
 
 
-    public static <F, K, V> Map<K, V> convert2NormalMap(List<F> list, Function<F, K> keyFunction,
-                                                        Function<F, V> valueFunction) {
-        if (isEmpty(list)) {
+    public static <K, V, T> Map<K, List<V>> convert2MapList(Collection<T> input, Function<T, K> keyFunction,
+                                                            Function<T, V> valueFunction) {
+        if (isEmpty(input)) {
             return Collections.EMPTY_MAP;
         }
 
-        Map map = new HashMap<>(list.size());
-        for (F en : list) {
-            K k = keyFunction.apply(en);
-            V v = valueFunction.apply(en);
-            map.put(k, v);
-        }
-        return map;
-    }
-
-
-    /**
-     * 使用function获取 F中的值作为key,同时将F加入map中的value list中
-     *
-     * @param list
-     * @param function
-     * @param <K>
-     * @param <F>
-     * @return
-     */
-    public static <K, F> Map<K, List<F>> convert2MapList(List<F> list, Function<F, K> function) {
-        if (isEmpty(list)) {
-            return Collections.EMPTY_MAP;
-        }
-        Map<K, List<F>> map = new HashMap<>();
-        for (F en : list) {
-            K t = function.apply(en);
-            List<F> res = map.get(t);
-            if (res != null) {
-                res.add(en);
+        Map<K, List<V>> res = new HashMap<>(input.size());
+        for (T entity : input) {
+            if (entity == null) {
+                continue;
+            }
+            K k = keyFunction.apply(entity);
+            V v = valueFunction.apply(entity);
+            List<V> tempValues = res.get(k);
+            if (tempValues == null) {
+                res.put(k, Lists.newArrayList(v));
             } else {
-                map.put(t, Lists.newArrayList(en));
+                tempValues.add(v);
             }
         }
-        return map;
+        return res;
     }
 
-    /**
-     * 使用keyFunction 获取 map的key,使用valueFunction获取 map的value
-     *
-     * @param list
-     * @param keyFunction
-     * @param valueFunction
-     * @param <K>
-     * @param <V>
-     * @param <F>
-     * @return
-     */
-    public static <K, V, F> Map<K, List<V>> convert2MapList(List<F> list, Function<F, K> keyFunction,
-                                                            Function<F, V> valueFunction) {
-        if (isEmpty(list)) {
-            return Collections.EMPTY_MAP;
-        }
-        Map<K, List<V>> map = new HashMap<>();
-        for (F en : list) {
-            K key = keyFunction.apply(en);
-            V value = valueFunction.apply(en);
-            List<V> res = map.get(key);
-            if (res != null) {
-                res.add(value);
-            } else {
-                map.put(key, Lists.newArrayList(value));
-            }
-        }
-        return map;
-    }
-
-
-    public static void main(String[] args) {
-        Person p1 = new Person("p1", 1, "a1");
-        Person p11 = new Person("p1", 1, "a11");
-        Person p2 = new Person("p2", 1, "a2");
-        Person p3 = new Person("p3", 1, "a3");
-        Person p4 = new Person("p4", 1, "a4");
-
-
-        List<Person> people = Lists.newArrayList(p1, p11, p2, p3, p4);
-
-        System.out.println(convert2MapList(people, new Function<Person, String>() {
-            @Override
-            public String apply(Person input) {
-                return input.name;
-            }
-        }));
-
-        System.out.println("====================");
-
-        System.out.println(convert2MapList(people, new Function<Person, String>() {
-            @Override
-            public String apply(Person input) {
-                return input.name;
-            }
-        }, new Function<Person, String>() {
-            @Override
-            public String apply(Person input) {
-                return input.address;
-            }
-        }));
-
-    }
-
-
-    static class Person {
-        String name;
-        Integer age;
-        String address;
-
-        public Person() {
-        }
-
-        public Person(String name, Integer age, String address) {
-            this.name = name;
-            this.age = age;
-            this.address = address;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Person person = (Person) o;
-            return Objects.equals(name, person.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name);
-        }
-    }
 }
